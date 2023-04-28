bf0743dfa132ed03b63184f2c48e24e2f6e211fa
Artur <zkhomers@gmail.com>
2023-04-28 23:15:11 +0300

    check

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ app.use(cors({
 }));
 
 app.get('/scrape/:page', async (req, res) => {
-
     const pageNumber = parseInt(req.params.page);
 
     try {
@@ -18,38 +17,24 @@ app.get('/scrape/:page', async (req, res) => {
             headless: 'new',
             args: ['--no-sandbox'],
         });
-        const page = await browser.newPage();
-        await page.goto(`https://hard.rozetka.com.ua/ua/computers/c80095/page=${pageNumber}`);
 
-        await page.evaluate(async () => {
-            await new Promise((resolve) => {
-                let totalHeight = 0;
-                const distance = 100;
-                const scrollInterval = setInterval(() => {
-                    const scrollHeight = document.body.scrollHeight;
-                    window.scrollBy(0, distance);
-                    totalHeight += distance;
-                    if (totalHeight >= scrollHeight) {
-                        clearInterval(scrollInterval);
-                        resolve();
-                    }
-                }, 100);
-            });
-        });
+        const page = await browser.newPage();
+
+        await page.goto(`https://hard.rozetka.com.ua/ua/computers/c80095/page=${pageNumber}`, { waitUntil: 'networkidle0' });
+
         const products = await page.evaluate(() => {
             const productList = [];
 
             const productNodes = document.querySelectorAll('.catalog-grid__cell');
 
             productNodes.forEach((productNode) => {
-                console.log('it for loop')
                 const name = productNode.querySelector('.goods-tile__title').textContent.trim();
                 const price = productNode.querySelector('.goods-tile__price-value').textContent.trim();
 
-                productList.push({name, price});
+                productList.push({ name, price });
             });
 
-            return {productList};
+            return productList;
         });
 
         await browser.close();
@@ -59,7 +44,6 @@ app.get('/scrape/:page', async (req, res) => {
         res.status(500).send('An error occurred while scraping the page');
     }
 });
-
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
