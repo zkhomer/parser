17d8f36903bea8796c7715d7a9e2a7c45878c7bd
Artur <zkhomers@gmail.com>
2023-04-28 23:49:49 +0300

    check

--- server/index.js
+++ server/index.js
@@ -9,28 +9,29 @@ app.use(cors({
 }));
 
 app.get('/scrape/:page', async (req, res) => {
+
     const pageNumber = parseInt(req.params.page);
+
     try {
         const browser = await puppeteer.launch({
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-accelerated-2d-canvas',
-                '--disable-gpu'
-            ]
+            executablePath: '/usr/bin/google-chrome',
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
-        await page.goto(`https://hard.rozetka.com.ua/ua/computers/c80095/page=${pageNumber}`);
+        await page.goto(`https://hard.rozetka.com.ua/ua/computers/c80095/page=${pageNumber}`, {waitUntil: 'networkidle0'});
 
         const products = await page.evaluate(() => {
             const productList = [];
             const productNodes = document.querySelectorAll('.catalog-grid__cell');
+
             productNodes.forEach((productNode) => {
                 const name = productNode.querySelector('.goods-tile__title').textContent.trim();
                 const price = productNode.querySelector('.goods-tile__price-value').textContent.trim();
+
                 productList.push({name, price});
             });
+
             return {productList};
         });
 
