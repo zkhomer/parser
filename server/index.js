a70132fe75bddeb9de94437da5f4aee0504a943a
Artur <zkhomers@gmail.com>
2023-04-28 17:27:38 +0300

    check

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.get('/scrape/:page', async (req, res) => {
 
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/google-chrome',
-        headless: 'new'
+        headless: true
     });
     const page = await browser.newPage();
     await page.goto(`https://hard.rozetka.com.ua/ua/computers/c80095/page=${pageNumber}`);
