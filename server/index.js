0e8ae80d646388f6c35337ea4db4afd0913096a4
Artur <zkhomers@gmail.com>
2023-04-28 23:46:18 +0300

    check

--- server/index.js
+++ server/index.js
@@ -9,33 +9,28 @@ app.use(cors({
 }));
 
 app.get('/scrape/:page', async (req, res) => {
-
     const pageNumber = parseInt(req.params.page);
-
     try {
         const browser = await puppeteer.launch({
-            executablePath: '/usr/bin/google-chrome',
-            headless: "new",
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--disable-gpu'
+            ]
         });
         const page = await browser.newPage();
         await page.goto(`https://hard.rozetka.com.ua/ua/computers/c80095/page=${pageNumber}`);
 
-
-
         const products = await page.evaluate(() => {
             const productList = [];
-             page.waitForSelector('.catalog-grid__cell');
             const productNodes = document.querySelectorAll('.catalog-grid__cell');
-
             productNodes.forEach((productNode) => {
-                console.log('it for loop')
                 const name = productNode.querySelector('.goods-tile__title').textContent.trim();
                 const price = productNode.querySelector('.goods-tile__price-value').textContent.trim();
-
                 productList.push({name, price});
             });
-
             return {productList};
         });
 
@@ -46,7 +41,6 @@ app.get('/scrape/:page', async (req, res) => {
         res.status(500).send('An error occurred while scraping the page');
     }
 });
-
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
