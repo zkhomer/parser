6c5ecce5dc1a1d305152141d246be548e7973e9f
Artur <zkhomers@gmail.com>
2023-05-01 23:07:52 +0300

    check

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ app.get('/scrape/:page', async (req, res) => {
 
     try {
         const browser = await puppeteer.launch({
-            // executablePath: '/usr/bin/google-chrome',
-            // headless: "new",
-            // args: ['--no-sandbox', '--disable-setuid-sandbox']
+            executablePath: '/usr/bin/google-chrome',
+            headless: "new",
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
         page.setDefaultNavigationTimeout(0);
