9b113c00891d98d415efd2b017a2a28b18662e70
Artur <zkhomers@gmail.com>
2023-05-01 23:07:40 +0300

    check

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ app.get('/scrape/:page', async (req, res) => {
 
     try {
         const browser = await puppeteer.launch({
-            executablePath: '/usr/bin/google-chrome',
-            headless: "new",
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            // executablePath: '/usr/bin/google-chrome',
+            // headless: "new",
+            // args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
         page.setDefaultNavigationTimeout(0);
@@ -37,7 +37,10 @@ app.get('/scrape/:page', async (req, res) => {
         });
 
         await browser.close();
-        res.send(products);
+       setTimeout(() => {
+           res.send(products);
+       }, 30000)
+
     } catch (error) {
         console.error(error);
         res.status(500).send('An error occurred while scraping the page');
