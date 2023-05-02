b36d415a4c0bf6635205b31ec2d5e4e2218ce44e
Artur <zkhomers@gmail.com>
2023-05-02 19:50:25 +0300

    check

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,4 @@ import LeftMenu from './views/LeftMenu.vue';
     <router-view></router-view>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
