f45a21fd8b7dc8ef9409660b34b8439b940ce1d5
Artur <zkhomers@gmail.com>
2023-05-12 19:31:24 +0300

    clear code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,13 @@
   <LeftMenu />
   <PageList />
   </div>
-<!--  <div class="maincontent">-->
-<!--    <ul v-if="productList.length !== 0">-->
-<!--      <li v-for="(product, index) in productList" :key="index">-->
-<!--        <img class="product-img" :src="product.img" alt="" />-->
-<!--        {{ product.name }} - {{ product.price }}-->
-<!--      </li>-->
-<!--    </ul>-->
-<!--    <div v-else>-->
-<!--      Parsing new page ...-->
-<!--    </div>-->
-<!--  </div>-->
 </template>
 
 <script>
-import axios from "axios";
+
 import LeftMenu from "./LeftMenu.vue";
 import PageList from './PageList.vue'
-import { useStore } from "../store/index";
+
 
 export default {
   components: { LeftMenu, PageList },
@@ -30,17 +19,6 @@ export default {
       page: 1,
       storeList:[]
     };
-  },
-  mounted() {
-    axios
-        .get("http://ec2-16-170-86-192.eu-north-1.compute.amazonaws.com/api", {
-        })
-        .then((response) => {
-          this.productList = response.data.productList;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
   },
   methods: {},
 };
