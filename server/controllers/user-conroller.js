0b1c67c1e1d65eebdb7283277651625c1e6c8a5d
Artur <zkhomers@gmail.com>
2023-07-21 22:14:29 +0300

    update: category info query

--- server/controllers/user-conroller.js
+++ server/controllers/user-conroller.js
@@ -54,6 +54,7 @@ const categoryDataController = async (req, res) => {
     try {
         const { cardSelector, customSelectors, pageUrl } = req.body;
         const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome',
             headless: "new",
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
