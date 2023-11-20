9b4c60f8062ce2ef829428833ea09595dea225db
Artur <zkhomers@gmail.com>
2023-11-20 12:22:58 +0200

    config port

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   base: "/home/",
   plugins: [vue()],
+  server: {
+    port: 8080,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
